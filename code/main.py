b27b3d163fab48aa1a6d8ca956bb30fa261ce0b8
ycchia <joshuaaaaa0429@gmail.com>
2024-05-02 00:46:13 +0800

    0430上課內容 做完了GameObject物件 在main檔

---
+++ code/main.py
@@ -0,0 +1,34 @@
+import pygame
+from pathlib import Path
+
+parent_path = Path(__file__).parents[1]
+image_path = parent_path/'res'
+icon_path = image_path/'airplaneicon.png'
+
+pygame.init()
+
+screenHigh = 760
+screenWidth = 1000
+
+playground = [screenHigh, screenWidth]
+screen = pygame.display.set_mode((screenWidth, screenHigh))
+pygame.display.set_caption("1942偽")
+icon = pygame.image.load(icon_path)
+pygame.display.set_icon(icon)
+background = pygame.Surface(screen.get_size())
+background = background.convert()
+background.fill((50, 50, 50))
+
+running = True
+fps = 120
+clock = pygame.time.Clock()
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    screen.blit(background, (0, 0))
+    pygame.display.update()
+    dt = clock.tick(fps)
+
+pygame.quit()
